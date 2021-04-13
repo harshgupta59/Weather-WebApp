35f36e7aa6439844060bbad607a2df9ee852a1e0
Harsh Gupta <hg.harshgupta1810@gmail.com>
2021-04-13 13:03:59 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+ window.addEventListener('load',()=>{
+     let long;
+     let lat;
+     let temperatureDescription = document.querySelector('.temperature-description')
+     let temperatureDegree = document.querySelector('.temperature-degree-value');    
+     let locationTimezone = document.querySelector('.location-timezone');
+     let icon=document.querySelector('#icon');
+     let temperaturetype=document.querySelector('.temperature-degree span');
+     const temperaturesection=document.querySelector('.temperature-degree');
+
+     if(navigator.geolocation){
+         
+         navigator.geolocation.getCurrentPosition(position=>{
+            long=position.coords.longitude;
+            lat=position.coords.latitude;
+            
+            const APIkey='836881cfa7d610e726d05b08947dd459';
+            const API=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${APIkey}`;
+            fetch(API)
+            .then(response => response.json())
+            .then(data => {
+                
+                //getting data fromAPI
+                const temperature=(data.main.temp-273.15).toFixed(2);
+                const description=data.weather[0].description;
+                country_name=data.sys.country;
+                const city_name=data.name;
+                const timezone=country_name+' / '+data.name;
+
+                //Set DOM elements from API
+                temperatureDegree.textContent=temperature;
+                temperatureDescription.textContent=description;
+                locationTimezone.textContent=timezone;
+                
+                //set icon
+                const iconID=data.weather[0].icon;
+                icon.src="icons/"+iconID+".png";
+                
+                //conversion formula 
+                let fahrenheit=(temperature) * (1.8) + 32;
+
+                //Temperature conversion
+                temperaturesection.addEventListener('click',()=>{
+                    console.log(temperaturetype);
+                    if(temperaturetype.textContent==="F"){
+                        temperaturetype.textContent="*C";
+                        temperatureDegree.textContent=temperature;
+                    }
+                    else{
+                        temperaturetype.textContent="F";
+                        temperatureDegree.textContent=fahrenheit;
+                    }
+                });
+            });
+         });
+     }
+ });
